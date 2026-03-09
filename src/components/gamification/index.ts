d40a02e0de3900488a3db7ec550c28e1550e97a5
Zhekan52 <kovalikeugeni@yandex.ru>
2026-03-09 15:19:32 +0700

    Delete src/components/gamification directory

--- src/components/gamification/index.ts
+++
@@ -1,4 +0,0 @@
-export { ProgressBar, MultiProgressBar, LevelProgress } from './ProgressBar';
-export { AchievementsPanel, AchievementsSummary } from './Achievements';
-export { Leaderboard, LeaderboardSummary } from './Leaderboard';
-export { GamificationPanel, GamificationWidget } from './GamificationPanel';
